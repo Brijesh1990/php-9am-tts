ceb203cfe61e9e82437ed0316fb8bd9462b8a356
Brijesh1990 <bkpandey.pandey@gmail.com>
2024-05-18 10:04:45 +0530

    18-05-2024 all data uploaded

---
+++ module1/html/html-head-tag/js/add.js
@@ -0,0 +1,23 @@
+function add()
+{
+    var a=parseInt(prompt("Enter a values :"));
+    var b=parseInt(prompt("Enter b values :"));
+    var c=a+b;
+    alert('Additions of numbers is :'+c);
+}
+
+function subs()
+{
+    var a=parseInt(prompt("Enter a values :"));
+    var b=parseInt(prompt("Enter b values :"));
+    var c=a-b;
+    alert('Substractions of numbers is :'+c);
+}
+
+function mult()
+{
+    var a=parseInt(prompt("Enter a values :"));
+    var b=parseInt(prompt("Enter b values :"));
+    var c=a*b;
+    alert('Multiplications of numbers is :'+c);
+}
